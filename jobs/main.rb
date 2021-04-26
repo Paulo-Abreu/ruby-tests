384968f5e3f3205db6ba99bcd32ddf37160cb0dc
Paulo <paulorabreuxd@gmail.com>
2021-04-26 11:11:09 -0300

    products

--- jobs/main.rb
+++ jobs/main.rb
@@ -9,4 +9,11 @@ items = product.map{ |p| Sales_manager.create_sale_item(p)}
 sale = Sales_manager.create_sale(items)
 sale_json = Sales_manager.response_as_json(sale, items)
 
-puts "A sua compra de numero: #{sale.id}, tem o total de #{sale.total_products} produtos, com um preco total de #{sale.total_price"
+puts "A sua compra de numero: #{sale.id}, tem o total de #{sale.total_products} produtos, com um preco total de #{sale.total_price}"
+var = sale_json.dig(:price) || sale.total_price / sale.total_products
+puts "A media de perco dos produtos  de: #{var}"
+
+value = sale_json.dig(:items)
+arr = []
+value.map{|a|arr << a[:price]}
+puts  "Seu produto mais caro tem o valor de: #{arr.sort.reverse[0]}"
